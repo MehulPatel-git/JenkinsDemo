f650fb63f1aa4b2a11914d4ef623831b731333a3
MehulPatel-git <Mehul.Patel@lntinfotech.com>
2021-03-22 14:40:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JenkinsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DragonBall Super");
+            Console.WriteLine("Jujutsu Kaisen");
+            Console.ReadLine();
+        }
+    }
+}
